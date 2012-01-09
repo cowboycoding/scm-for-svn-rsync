a9af51ab1d97452a3f4ac157c92285dbd73871c9
rasmus <rasmus@5aa23cd9-f012-41f9-9c3b-4b5a37723f64>
2012-01-09 15:14:51 +0000

    added help
    git-svn-id: file:///srv/svn-repos/scm/trunk@39 5aa23cd9-f012-41f9-9c3b-4b5a37723f64

--- commands/Help.class.php
+++ commands/Help.class.php
@@ -12,7 +12,34 @@ Class Help
 		if(strlen($msg) > 0)
 			echo $msg."\n\n";
 
-		echo 'This is help';
+		$cmds = array(
+		
+		'--checkout-product' => array('args' => array('productname'), 'text' => 'Checkouts a product form the repository'),
+		'--release-product' => array('args' => array('productname', 'destination'), 'text' => 'Release a product trunk to destination'),
+		'--create-branch' => array('args' => array('productname', 'branchname'), 'text' => 'Creates a branch from product'),
+		'--checkout-product' => array('args' => array('productname', 'branchname'), 'text' => 'Checksout a products branch'),
+		'--sync-branch' => array('args' => array('productname', 'branchname'), 'text' => 'Synces a products branch with products trunk'),
+		'--merge-branch' => array('args' => array('productname', 'branchname'), 'text' => 'Merges a branch back into products trunk')
+
+		);
+
+		echo "\nSCM For svn+rsync, written in PHP by Rasmus Styrk\n";		
+		echo "\nCommnads:\n\n";
+
+		foreach($cmds as $cmd => $values)
+		{
+			$args = '';
+			
+			if(array_key_exists('args', $values))
+			{
+				foreach($values['args'] as $arg)
+					$args .= sprintf("<%s> ", $arg);
+			}
+
+			printf("\t%s\n\t\t%s\n\t\t%s\n\n", $cmd, $args, $values['text']);
+		}
+
+		echo "\n\n";
 
 		exit(0);
 	}
