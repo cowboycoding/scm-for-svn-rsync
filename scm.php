8b2deb60d50b7442b599930ffde8a1e82a5402a1
rasmus <rasmus@5aa23cd9-f012-41f9-9c3b-4b5a37723f64>
2012-01-09 10:38:00 +0000

    p
    git-svn-id: file:///srv/svn-repos/scm/trunk@25 5aa23cd9-f012-41f9-9c3b-4b5a37723f64

--- scm.php
+++ scm.php
@@ -1,5 +1,4 @@
 <?php
-
 // Include config
 require_once('Config.class.php');
 
@@ -9,6 +8,7 @@ require_once('helpers/User.class.php');
 
 // Commands
 require_once('commands/Products.class.php');
+require_once('commands/Branches.class.php');
 require_once('commands/Help.class.php');
 
 $method = @$argv[1];
@@ -16,8 +16,20 @@ switch($method)
 {
 	case '--checkout-product':
 		\commands\Products::checkout($argv);	
-	break;
+		break;
+	
+	case '--create-branch':
+		\commands\Branches::create($argv);	
+		break;
+	
+	case '--checkout-branch':
+		\commands\Branches::checkout($argv);
+		break;
 			
+	case '--sync-branch':
+		\commands\Branches::sync($argv);
+		break;
+
 	default:
 		\commands\Help::show();
 	break;	
